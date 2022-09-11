22e0664ce1c0091c7ee48ecd9a77e7f7176d7daf
Wiktor <wiktorkoscielny@gmail.com>
2022-09-11 22:16:12 +0200

    I extended the way a function works to check if the arguments are empty or contain only one value

--- src/index.js
+++ src/index.js
@@ -13,7 +13,32 @@ const path = require("path");
 
 function count(users, mobileDevices, iotDevices) {
   let result;
-  if (Object.keys(users).length === 0) console.error('There is no users data') // first of all check if there is any user data - to quit function earlier if there is no any and make whole code faster 
+  const [usersKeys, mobileKeys, iotkeys] = [Object.keys(users).length, Object.keys(mobileDevices).length, Object.keys(iotDevices).length]
+
+  // first condition
+  if (usersKeys === 0 || mobileKeys === 0 || iotkeys === 0) return 'There is no user data'
+
+  // second condition
+  if (usersKeys === 1) {
+    const mdFiltered = mobileKeys === 0 ? mobileDevices[0].user : mobileDevices.filter(obj => obj.user === users.id)
+    let idFiltered
+    if (mobileKeys === 0) return idFiltered = iotkeys === 0 ? iotDevices[0].filter(obj => obj.mobile === mobileDevices[0].id) : iotDevices.filter(obj => obj.mobile === mobileDevices[0].id)
+    mdFiltered.map(item => { return idFiltered = iotDevices.filter(obj => obj.mobile === item.id) })
+    let length
+    if (idFiltered === undefined || idFiltered.length === undefined || idFiltered.length === 0) {
+      length = 0
+    } else if (idFiltered.length > 0) {
+      const newData = Object.assign({}, idFiltered),
+        dataLength = Object.keys(newData).length
+      let sum = 0
+      for (let n = 0; n < dataLength; n++) {
+        sum += newData[n].length
+      }
+      length = sum
+    } else length = 1
+  }
+
+  // third condition
   const resultOfMapping = users.map(i => {
     const mdFiltered = mobileDevices.filter(obj => obj.user === i.id)
     let idFiltered = mdFiltered.map(item => {
@@ -36,3 +61,4 @@ function count(users, mobileDevices, iotDevices) {
   })
   return result = resultOfMapping
 }
+module.exports = count
