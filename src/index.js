ac93d09ffd274c93648d5e9fc71453b8b83e582a
Wiktor <wiktorkoscielny@gmail.com>
2022-09-11 21:08:00 +0200

    Had some misunderstanding with task guidelines and had to rewrite some lines

--- src/index.js
+++ src/index.js
@@ -14,26 +14,25 @@ const path = require("path");
 function count(users, mobileDevices, iotDevices) {
   let result;
   if (Object.keys(users).length === 0) console.error('There is no users data') // first of all check if there is any user data - to quit function earlier if there is no any and make whole code faster 
-    const resultOfMapping = users.map(i => {
-      // mobile data
-        const mobileData = mobileDevices.map(item => item.user),
-          findUserMobileData = mobileData.filter(item => item === i.id),
-          sumOfMobileDevices = findUserMobileData.length
-      // iot data
-        const mdFiltered = mobileDevices.filter(obj => obj.user === i.id)
-        let idFiltered =  mdFiltered.map(item => {
-          return iotDevices.filter(obj => obj.mobile === item.id)
-        })
-        let length
-        if (idFiltered.length === undefined || idFiltered.length === 0) {
-          length = 0
-        } else if (idFiltered.length > 0) {
-          [a] = idFiltered, b = JSON.stringify(a), c = JSON.parse(b)
-          d = c.map(i => i.id)
-          length = d.length + 1
-        } else length = 1
-          const sumOfDevices = sumOfMobileDevices + length
-        return i.name + ' => ' + sumOfDevices
+  const resultOfMapping = users.map(i => {
+    const mdFiltered = mobileDevices.filter(obj => obj.user === i.id)
+    let idFiltered = mdFiltered.map(item => {
+      return iotDevices.filter(obj => obj.mobile === item.id)
     })
-    return result = resultOfMapping
+    let length
+    if (idFiltered.length === undefined || idFiltered.length === 0) {
+      length = 0
+    } else if (idFiltered.length > 0) {
+      const newData = Object.assign({}, idFiltered),
+        dataLength = Object.keys(newData).length
+      let sum = 0
+      for (let n = 0; n < dataLength; n++) {
+        sum += newData[n].length
+      }
+      length = sum
+    } else length = 1
+    const sumOfDevices = length
+    return i.name + ' => ' + sumOfDevices
+  })
+  return result = resultOfMapping
 }
